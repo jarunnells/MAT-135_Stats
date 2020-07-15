cd1d77d999cdff4179d916380d1c79d8986a851f
J.A. Runnells-Linux <48547460+jarunnells@users.noreply.github.com>
2020-07-14 21:46:14 -0600

    [functionTest.js] refactor: JSDoc comments

--- functionsTest.js
+++ functionsTest.js
@@ -1,26 +1,10 @@
 /*
-
     Google Sheets:
         [1] https://www.makeuseof.com/tag/create-custom-functions-google-sheets/
         [2] https://developers.google.com/apps-script/guides/sheets/functions
 
+        x̄  p̂  α
 */
-// x̄  p̂  α 
-
-
-function phat(x,n) {
-
-    var phat = x/n;
-
-    return phat;
-}
-
-function marginErr(z_alpha,phat,n) {
-
-    var marginErr = (z_alpha/2)*Math.sqrt((phat*(1-phat))/n);
-
-    return marginErr;
-}
 
 /*===========================================================================*/
 
@@ -30,23 +14,21 @@ function marginErr(z_alpha,phat,n) {
  * @param {number} x - individuals in the sample with a specified characteristic.
  * @param {number} n - sample size
  * @return Number of individuals divided by sample size.
- * @customfunction 
+ * @customfunction
  */
-function PHAT(x,n) {    
-
+function PHAT(x,n) {
     return x/n;
 }
 
 /**
  * Calculates the margin of error [E or ME] for a proportion.
  * @constructor
- * @param {number} z_α - [z sub alpha] 
- * @param {number} p̂ - [p hat] 
+ * @param {number} z_alpha2 - z_α/2 [z sub alpha divided by 2]
+ * @param {number} phat- p̂ (population proportion)
  * @param {number} n - sample size
  * @return The value of z_α divided by two, then multiplied by the square root of the product of p hat and the complement of p hat divided by the sample size.
  * @customfunction
  */
 function MARGINERROR(z_alpha2,phat,n) {
-
     return z_alpha2*Math.sqrt((phat*(1-phat))/n);
 }
