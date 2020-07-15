08e0e45ec9670d06d4a46b9f88cc6479be3850cd
J.A. Runnells-Linux <48547460+jarunnells@users.noreply.github.com>
2020-07-15 03:13:16 -0600

    [functionTest.js] refactor: added confidence interval method

--- functionsTest.js
+++ functionsTest.js
@@ -9,6 +9,18 @@
 /*===========================================================================*/
 
 /**
+ * Calculates standard deviation of the sampling distribution of p̂ (population proportion) from given X and n.
+ * @constructor
+ * @param {number} p - population proportion.
+ * @param {number} n - sample size
+ * @return The square root of the product of population proportion and its complement divided by the sample size.
+ * @customfunction
+ */
+function STDEV_PHAT(p,n) {
+    return Math.sqrt((p*(1-p))/n);
+}
+
+/**
  * Calculates p̂ (population proportion) from given X and n.
  * @constructor
  * @param {number} x - individuals in the sample with a specified characteristic.
@@ -26,9 +38,29 @@ function PHAT(x,n) {
  * @param {number} z_alpha2 - z_α/2 [z sub alpha divided by 2]
  * @param {number} phat - p̂ (population proportion)
  * @param {number} n - sample size
- * @return The value of z_α divided by two, then multiplied by the square root of the product of p hat and the complement of p hat divided by the sample size.
+ * @return The value of z_α divided by two, then multiplied by
  * @customfunction
  */
-function MARGINERROR(z_alpha2,phat,n) {
+function MARGIN_ERROR(z_alpha2,phat,n) {
     return z_alpha2*Math.sqrt((phat*(1-phat))/n);
 }
+
+/**
+ * Calculates the specified upper or lower bound value.
+ * @constructor
+ * @param {number} z_alpha2 - z_α/2 [z sub alpha divided by 2]
+ * @param {number} phat - p̂ (population proportion)
+ * @param {number} n - sample size
+ * @param {boolean} lower - true for lower bound, false for upper bound.
+ * @return The
+ * @customfunction
+ */
+function CONFIDENCE_INTERVAL(z_alpha2,phat,n,lower) {
+    switch (lower) {
+        case true:
+            return phat - MARGINERROR(z_alpha2,phat,n);
+        case false:
+            return phat + MARGINERROR(z_alpha2,phat,n);
+    }
+}
+/**/
