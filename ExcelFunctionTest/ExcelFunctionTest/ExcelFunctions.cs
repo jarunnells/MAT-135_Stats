8c65a21ff5d9c08ff262ca5df9af0cba31c81d6b
J.A. Runnells-Win <48547460+jarunnells@users.noreply.github.com>
2020-07-18 02:53:33 -0600

    init add ExcelDNA test proj to repo

---
+++ ExcelFunctionTest/ExcelFunctionTest/ExcelFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace ExcelFunctionTest
+{
+    public static class ExcelFunctions
+    {
+        [ExcelFunction(Description = ".NET Function Test - Hello...")]
+        public static string SayHello([ExcelArgument(Name = "name", Description = "A name...")] string name)
+        {
+            return $"Hello {name}";
+        }
+
+        [ExcelFunction(Description = ".NET Function Test - AddNums(n1,n2)")]
+        public static double AddNums(
+            [ExcelArgument(Name = "num1", Description = "First number to add")] double n1,
+            [ExcelArgument(Name = "num2", Description = "Second number to add")] double n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
