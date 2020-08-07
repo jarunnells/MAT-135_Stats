99fe8e4c8c818d9d5a0512338fd15f668893864d
J.A. Runnells-Win <48547460+jarunnells@users.noreply.github.com>
2020-08-07 14:45:18 -0600

    minor refactor - cleanup

--- ExcelFunctionTest/ExcelFunctionTest/ExcelFunctions.cs
+++ ExcelFunctionTest/ExcelFunctionTest/ExcelFunctions.cs
@@ -35,34 +35,26 @@ namespace ExcelFunctionTest
     {
         [ExcelFunction(Description = ".NET Function Test - Hello...")]
         public static string SayHello([ExcelArgument(Name = "name", Description = "A name...")] string name)
-        {
-            return $"Hello {name}!!";
-        }
+        { return $"Hello {name}!!"; }
 
         [ExcelFunction(Description = ".NET Function Test - AddNums(n1,n2)")]
         public static double MINUS(
             [ExcelArgument(Name = "num1", Description = "Number to subtract from.")] double n1,
             [ExcelArgument(Name = "num2", Description = "Number being subtracted.")] double n2)
-        {
-            return n1 - n2;
-        }
+        { return n1 - n2; }
 
         [ExcelFunction(Description = "Calculates p̂ (population proportion) from given X and n values.")]
         public static double PHAT(
             [ExcelArgument(Name = "x", Description = "individuals in the sample with a specified characteristic")] double x,
             [ExcelArgument(Name = "n", Description = "sample size")] double n)
-        {
-            return x / n;
-        }
+        { return x / n; }
 
         // TODO: complete descriptions
         [ExcelFunction(Description ="Calculates standard deviation of phat")]
         public static double STDEV_PHAT(
             [ExcelArgument(Name = "phat", Description = "phat value description")] double phat,
             [ExcelArgument(Name = "n", Description = "sample size description")] double n)
-        {
-            return Math.Sqrt(phat * (1 - phat) / n);
-        }
+        { return Math.Sqrt(phat * (1 - phat) / n); }
 
         // TODO: complete descriptions
         [ExcelFunction(Description = "Calculates the margin of error [E or ME] for a proportion.")] 
@@ -70,9 +62,7 @@ namespace ExcelFunctionTest
             [ExcelArgument(Name = "z_alpha2", Description ="")] double z_alpha2,
             [ExcelArgument(Name = "phat", Description ="")] double phat,
             [ExcelArgument(Name = "n", Description ="")] double n)
-        {
-            return z_alpha2 * Math.Sqrt(phat * (1 - phat) / n);
-        }
+        { return z_alpha2 * Math.Sqrt(phat * (1 - phat) / n); }
 
         // TODO: complete descriptions
         [ExcelFunction(Description = "Calculates the margin of error [E or ME] for a proportion.")]
@@ -80,9 +70,7 @@ namespace ExcelFunctionTest
             [ExcelArgument(Name = "t_alpha2", Description = "")] double t_alpha2,
             [ExcelArgument(Name = "s", Description = "")] double s,
             [ExcelArgument(Name = "n", Description = "")] double n)
-        {
-            return t_alpha2 * (s / Math.Sqrt(n));
-        }
+        { return t_alpha2 * (s / Math.Sqrt(n)); }
 
         // TODO: complete switch/case implementation
         [ExcelFunction(Description = "Calculates the margin of error [E or ME] for a proportion.")]
@@ -91,24 +79,20 @@ namespace ExcelFunctionTest
             [ExcelArgument(Name = "phat", Description = "")] double s,
             [ExcelArgument(Name = "n", Description = "")] double n,
             [ExcelArgument(Name = "lower", Description = "")] bool lower = false)
-        {
-            return 0;
-        }
+        { return 0; }
 
-        public static string TestDefault(object nameArg)
-        {
-            string name = Optional.Check(nameArg, " Unknown Person!?");
-            return $"Hello {name}";
-        }
+        // TODO: complete descriptons
+        [ExcelFunction(Description = "Function to test Optional helper class implementation.")]
+        public static string TestDefault(
+            [ExcelArgument(Name = "nameArg", Description = "")] object nameArg)
+        { return $"Hello {Optional.Check(nameArg, " Unknown Person!?")}"; }
     }
 
     // Helper Class - Optionals
     internal static class Optional
     {
         internal static bool Check(object arg, bool defaultVal)
-        {
-            return defaultVal;
-        }
+        { return defaultVal; }
 
         internal static int Check(object arg, int defaultVal)
         {
